7da5559510016fbb84979cc44184c8367559f14e
hyun <rlaalgus132@naver.com>
2020-06-03 19:51:05 +0900

    Updates

---
+++ precache-manifest.41db83a6377dcd1659acc9a33c42d602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6963d8a57c5eb82cb9356f4ef7dc118b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f1be8ec84b335067937e",
+    "url": "/movie_app/static/css/main.8c5089d2.chunk.css"
+  },
+  {
+    "revision": "778cd1dc29ef15ee9de9",
+    "url": "/movie_app/static/js/2.71f3439d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.71f3439d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1be8ec84b335067937e",
+    "url": "/movie_app/static/js/main.eb61a9b0.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
